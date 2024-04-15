4b03077eae39fdd319f0a94994dd2f7aba7a56e5
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-04-15 10:53:43 +0800

    Update AppabeticalApp.swift

--- Appabetical/AppabeticalApp.swift
+++ Appabetical/AppabeticalApp.swift
@@ -24,7 +24,7 @@ struct AppabeticalApp: App {
             .onAppear {
                 checkNewVersions()
                 if isiPad() {
-                    UIApplication.shared.alert(title: "Warning", body: "Appabetical does not support iPad yet! Please do not use the app as there may be unexpected side effects.")
+                    UIApplication.shared.alert(title: "提示", body: "Appabetical还不支持iPad! 请不要使用该应用程序，可能会出现未知问题。")
                 }
                 checkAndEscape()
             }
@@ -45,23 +45,23 @@ struct AppabeticalApp: App {
 #else
         var supported = false
         var needsTrollStore = false
-        if #available(iOS 16.2, *) {
+        if #available(iOS 16.7, *) {
             supported = false
         } else if #available(iOS 16.0, *) {
             supported = true
-            needsTrollStore = false
-        } else if #available(iOS 15.7.2, *) {
+            needsTrollStore = true
+        } else if #available(iOS 15.8.2, *) {
             supported = false
         } else if #available(iOS 15.0, *) {
             supported = true
-            needsTrollStore = false
+            needsTrollStore = true
         } else if #available(iOS 14.0, *) {
             supported = true
             needsTrollStore = true
         }
         
         if !supported {
-            UIApplication.shared.alert(title: "Not Supported", body: "This version of iOS is not supported. Please close the app.")
+            UIApplication.shared.alert(title: "不支持", body: "不支持此版本的iOS，请关闭应用程序。")
             return
         }
             
@@ -70,13 +70,13 @@ struct AppabeticalApp: App {
             try FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: "/var/mobile"), includingPropertiesForKeys: nil)
         } catch {
             if needsTrollStore {
-                UIApplication.shared.alert(title: "Use TrollStore", body: "You must install this app with TrollStore for it to work with this version of iOS. Please close the app.")
+                UIApplication.shared.alert(title: "使用 TrollStore", body: "您必须将此应用程序与TrollStore一起安装，以便与此版本的iOS一起工作，请关闭应用程序。")
                 return
             }
             // Use MacDirtyCOW to gain r/w
             grant_full_disk_access() { error in
                 if (error != nil) {
-                    UIApplication.shared.alert(body: "\(String(describing: error?.localizedDescription))\nPlease close the app and retry.")
+                    UIApplication.shared.alert(body: "\(String(describing: error?.localizedDescription))\n请关闭应用程序并重试。")
                     return
                 }
             }
@@ -87,14 +87,14 @@ struct AppabeticalApp: App {
     // Credit to SourceLocation
     // https://github.com/sourcelocation/AirTroller/blob/main/AirTroller/AirTrollerApp.swift
     func checkNewVersions() {
-        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let url = URL(string: "https://api.github.com/repos/Avangelista/Appabetical/releases/latest") {
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let url = URL(string: "https://api.github.com/repos/wwg135/Appabetical/releases/latest") {
             let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
                 guard let data = data else { return }
                 
                 if let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
                     if (json["tag_name"] as? String)?.compare(version, options: .numeric) == .orderedDescending {
-                        UIApplication.shared.confirmAlert(title: "Update Available", body: "A new version of Appabetical is available. It is recommended you update to avoid encountering bugs. Would you like to view the releases page?", onOK: {
-                            UIApplication.shared.open(URL(string: "https://github.com/Avangelista/Appabetical/releases/latest")!)
+                        UIApplication.shared.confirmAlert(title: "有更新可用", body: "Appabetical的新版本已经发布，建议您更新以避免遇到错误，您想查看发布页面吗?", onOK: {
+                            UIApplication.shared.open(URL(string: "https://github.com/wwg135/Appabetical/releases/latest")!)
                         }, noCancel: false)
                     }
                 }
